c57168c23b8ff0a3dbf2d27aa4caf402998f6dd2
ScottWei00 <981702664@qq.com>
2022-06-11 21:24:41 +0800

    Initial commit

---
+++ Source/Pacgame/Pacgame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pacgame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pacgame, "Pacgame" );
